bd2b52d933895ba9062ec5487b3d17218f2ac28a
Joseph H <josephh@yatosha.com>
2023-04-26 01:57:49 +0300

    Changed approach

--- rebuild-dns-on-newfiles.sh
+++ rebuild-dns-on-newfiles.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Monitor the /var/named directory for changes to .db files
-inotifywait -m /var/named -e modify --format '%f' --exclude '!*.db' |
-    while read file; do
-        # Execute the rebuilddnsconfig script when a .db file is modified
-        /scripts/rebuilddnsconfig
-    done
+# Get the modification time of the newest .db file in the /var/named directory
+newest_file=$(find /var/named -type f -name '*.db' -printf "%T@ %p\n" | sort -n | tail -1 | awk '{print $2}')
+newest_time=$(stat -c %Y "$newest_file")
+
+# Get the modification time of the last rebuild
+last_time=$(cat /var/run/rebuild-dns-config.time 2>/dev/null)
+
+# If the newest .db file has been modified since the last rebuild, rebuild the DNS configuration
+if [[ $newest_time -gt $last_time ]]; then
+    /scripts/rebuilddnsconfig
+    echo $newest_time > /var/run/rebuild-dns-config.time
+fi
