f1176d8303b403bda13beb528ac3f46ffab6cc03
Joseph H <josephh@yatosha.com>
2023-04-25 12:03:53 +0300

    Update easy-csf.sh

--- easy-csf.sh
+++ easy-csf.sh
@@ -5,14 +5,14 @@ RESET='\033[0m'
 
 # Check if csf is installed and uninstall if it is
 if [ -f "/etc/csf/csf.conf" ]; then
-    echo -e "${YELLOW}CSF Is installed. Uninstalling...${RESET}"
+    echo -e "${YELLOW}CSF is installed. Let's remove it first...${RESET}"
     cd /etc/csf
     sh uninstall.sh  > /dev/null 2>&1
 fi
 
 # Install csf if it is not installed
 if ! [ -f "/etc/csf/csf.conf" ]; then
-    echo -e "${YELLOW}Installing CSF${RESET}"
+    echo -e "${YELLOW}We are installing CSF${RESET}"
     cd /usr/src
     rm -fv csf.tgz
     wget https://download.configserver.com/csf.tgz
@@ -21,18 +21,18 @@ if ! [ -f "/etc/csf/csf.conf" ]; then
     sh install.sh  > /dev/null 2>&1
     cd
     echo ""
-    echo -e "${YELLOW}CSF installed${RESET}"
+    echo -e "${YELLOW}That was easy!! CSF installed Ok${RESET}"
 fi
 
 # Get port number from user input
 read -p "Enter port number to open: " PORT
 
 # Disable csf testing
-echo -e "${YELLOW}Disabling Test Mode${RESET}"
+echo -e "${YELLOW}Doing my magic, Test mode is removed${RESET}"
 sed -i "s/TESTING = \"1\"/TESTING = \"0\"/g" /etc/csf/csf.conf
 
 # Set RESTRICT_SYSLOG to 3
-echo -e "${YELLOW}Enable RESTRICT_SYSLOG${RESET}"
+echo -e "${YELLOW}Another trick now RESTRICT_SYSLOG is enabled by me!!${RESET}"
 sed -i "s/RESTRICT_SYSLOG = \"0\"/RESTRICT_SYSLOG = \"3\"/g" /etc/csf/csf.conf
 
 # Open port for incoming TCP traffic
@@ -42,10 +42,10 @@ sed -i "/^TCP_IN = /s/$PORT //g" /etc/csf/csf.conf
 sed -i "s/TCP_OUT = \"20,21,22,25,53,853,80,110,113,443,587,993,995,2222\"/TCP_OUT = \"20,21,22,25,53,853,80,110,113,443,587,993,995,2222,$PORT\"/g" /etc/csf/csf.conf
 
 # Update SSH port
-echo -e "${YELLOW}Chaning SSH Port to $PORT ${RESET}"
+echo -e "${YELLOW}Why leave default SSH Port? Setting it to port $PORT ${RESET}"
 /bin/sed -i "s/#Port 22/Port $PORT/g" /etc/ssh/sshd_config
 /bin/sed -i "s/Port 22/Port $PORT/g" /etc/ssh/sshd_config
-echo -e "${YELLOW}Done${RESET}"
+echo -e "${YELLOW}That was easy!! Done${RESET}"
 
 # Restart SSH
 service sshd restart
@@ -53,4 +53,4 @@ service sshd restart
 # Restart csf
 csf -r > /dev/null 2>&1
 
-echo -e "${YELLOW} CSF installed and $PORT opened successfully.${RESET}"
+echo -e "${YELLOW}All good now, CSF installed and I opened port $PORT successfully.${RESET}"
