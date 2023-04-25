9fb730ff8ab12f8c1b3144b3ee3c9f935d8358ee
Joseph H <josephh@yatosha.com>
2023-04-25 11:14:06 +0300

    Update easy-csf.sh

--- easy-csf.sh
+++ easy-csf.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Get port from user input
-read -p "Enter port number to allow: " PORT
+# Check if csf is installed and uninstall if it is
+if [ -f "/etc/csf/csf.conf" ]; then
+    echo -e "\e[32mCSF found. Uninstalling...\e[0m"
+    cd /etc/csf
+    sh uninstall.sh
+fi
 
-# Check if csf is installed
-if [ -d "/etc/csf" ]; then
-    echo "csf is already installed"
-else
+# Install csf if it is not installed
+if ! [ -f "/etc/csf/csf.conf" ]; then
+    echo -e "\e[32mCSF not found. Installing...\e[0m"
     cd /usr/src
     rm -fv csf.tgz
     wget https://download.configserver.com/csf.tgz
@@ -15,24 +18,29 @@ else
     sh install.sh
 fi
 
-# Enable port in csf
-if [ "$1" == "--disable-test" ]; then
-    sed -i "s/^TESTING = \"1\"/TESTING = \"0\"/" /etc/csf/csf.conf
-fi
-sed -i "s/^TCP_IN = \"/TCP_IN = \"$PORT,/" /etc/csf/csf.conf
-if grep -q "^TCP_OUT = \".*7162\"" /etc/csf/csf.conf; then
-    sed -i "s/^TCP_OUT = \".*7162\"/TCP_OUT = \"20,21,22,25,53,853,80,110,113,443,587,993,995,2222,$PORT\"/" /etc/csf/csf.conf
-else
-    sed -i "s/^TCP_OUT = \"/TCP_OUT = \"$PORT,/" /etc/csf/csf.conf
-fi
-csf -r
+# Get port number from user input
+read -p "Enter port number to open: " PORT
 
-# Enable port for ssh
-/bin/sed -i "s/#Port 22/Port $PORT/g" /etc/ssh/sshd_config
-/bin/sed -i "s/Port 22/Port $PORT/g" /etc/ssh/sshd_config
-service sshd restart
+# Disable csf testing
+sed -i "s/TESTING = \"1\"/TESTING = \"0\"/g" /etc/csf/csf.conf
 
 # Set RESTRICT_SYSLOG to 3
-sed -i "s/^RESTRICT_SYSLOG = \".*\"/RESTRICT_SYSLOG = \"3\"/" /etc/csf/csf.conf
+sed -i "s/RESTRICT_SYSLOG = \"2\"/RESTRICT_SYSLOG = \"3\"/g" /etc/csf/csf.conf
 
-echo "Port $PORT has been opened and ssh port has been changed to $PORT"
+# Open port for incoming TCP traffic
+sed -i "/^TCP_IN = /s/$PORT //g" /etc/csf/csf.conf
+
+# Open port for outgoing TCP traffic
+sed -i "s/TCP_OUT = \"20,21,22,25,53,853,80,110,113,443,587,993,995,2222\"/TCP_OUT = \"20,21,22,25,53,853,80,110,113,443,587,993,995,2222,$PORT\"/g" /etc/csf/csf.conf
+
+# Update SSH port
+/bin/sed -i "s/#Port 22/Port $PORT/g" /etc/ssh/sshd_config
+/bin/sed -i "s/Port 22/Port $PORT/g" /etc/ssh/sshd_config
+
+# Restart SSH
+service sshd restart
+
+# Restart csf
+csf -r
+
+echo -e "\e[32mPort $PORT opened successfully.\e[0m"
